71414978b5846e2eadce51833666ef76af184536
Clev <105460616+clevartist@users.noreply.github.com>
2026-08-19 12:11:25 +0300

    Change background images to black for ImageCanvas

--- src/components/ImageCanvas.js
+++ src/components/ImageCanvas.js
@@ -1,31 +1,26 @@
 import { forwardRef, useEffect, useRef } from "react";
-import albastru from "../chitanta_albastra.png";
-import rosu from "../chitanta_rosie.png";
-import verde from "../chitanta_verde.png";
+import negru from "../chitanta_black.png";
 import "../styles.css";
 
 const ImageCanvas = forwardRef(({ texts }, mainCanvasRef) => {
   const subCanvasRefs = useRef([]);
 
   const canvases = [
-    { rect: [0, 0, 1748, 982], bg: albastru },
-    { rect: [0, 1263, 1748, 982], bg: rosu }, // Updated position
-    { rect: [0, 2524, 1748, 982], bg: verde }, // Updated position
+    { rect: [0, 0, 1748, 982], bg: negru },
+    { rect: [0, 1263, 1748, 982], bg: negru },
+    { rect: [0, 2524, 1748, 982], bg: negru },
   ];
 
   useEffect(() => {
     const mainCanvas = mainCanvasRef.current;
     const mainCTX = mainCanvas.getContext("2d");
 
-    // Clear the main canvas
     mainCTX.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
 
-    // Draw each sub-canvas onto the main canvas
     canvases.forEach((config, index) => {
       const subCanvas = subCanvasRefs.current[index];
       const subCTX = subCanvas.getContext("2d");
 
-      // Clear and draw the background for the sub-canvas
       subCTX.clearRect(
         config.rect[0],
         config.rect[1],
@@ -40,7 +35,6 @@ const ImageCanvas = forwardRef(({ texts }, mainCanvasRef) => {
       backgroundImage.onload = () => {
         subCTX.drawImage(backgroundImage, 0, 0, config.rect[2], config.rect[3]);
 
-        // Loop through each text and draw it on the canvas
         texts.forEach(({ text, x, y, scale }) => {
           subCTX.fillStyle = "black";
           subCTX.font = scale;
@@ -48,7 +42,6 @@ const ImageCanvas = forwardRef(({ texts }, mainCanvasRef) => {
           subCTX.fillText(text, x, y);
         });
 
-        // Draw the sub-canvas onto the main canvas after the image is loaded
         mainCTX.drawImage(subCanvas, config.rect[0], config.rect[1]);
       };
     });
